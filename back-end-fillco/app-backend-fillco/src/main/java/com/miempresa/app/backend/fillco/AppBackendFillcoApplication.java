de727f8f410ac139fa2e0eb9c5bb77c8e8438d66
John Duarte <johnduarte7@gmail.com>
2019-09-05 18:10:02 -0500

    esto es un codigo nuevo

---
+++ back-end-fillco/app-backend-fillco/src/main/java/com/miempresa/app/backend/fillco/AppBackendFillcoApplication.java
@@ -0,0 +1,15 @@
+package com.miempresa.app.backend.fillco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBackendFillcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppBackendFillcoApplication.class, args);
+		
+	
+	}
+
+}
---
+++ back-end-fillco/app-backend-fillco/src/main/java/com/miempresa/app/backend/fillco/models/entity/Client.java
@@ -0,0 +1,71 @@
+package com.miempresa.app.backend.fillco.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "clients")
+public class Client implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	
+	private String name;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="create_at")
+	@Temporal(TemporalType.DATE)
+	private Date createAt;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	private static final long serialVersionUID = 1L;
+
+}
